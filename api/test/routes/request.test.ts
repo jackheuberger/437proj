64a80f809a39540b6628331337444d7c85e498e5
Jack Heuberger <jackheuberger5@gmail.com>
2024-02-15 13:53:25 -0600

    refactored tests

--- api/test/routes/request.test.ts
+++ api/test/routes/request.test.ts
@@ -3,28 +3,31 @@ import { describe, expect, it, test } from 'bun:test'
 import { Elysia } from 'elysia'
 import { request } from '../../src/routes/request'
 
-const createRequest = (body: any) => new Request('http://localhost/request', {
+const app = new Elysia().use(request)
+const createRequest = async (body: any, endpoint: string) => await app.handle(new Request(`http://localhost${endpoint}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(body)
-})
+}))
 
-const app = new Elysia().use(request)
+describe('/request', () => {
+    // Shadow definition of createRequest passing in the /request endpoint
+    const requestTest = async (body: any) => await createRequest(body, '/request')
 
-describe('RequestRoute', () => {
     test('should fail with no departure airport', async () => {
-        const response = await app.handle(createRequest({ to: 'LAX', date: '2022-01-01' }))
+        const response = await requestTest({ to: 'LAX', date: '2022-01-01' })
         expect(response.ok).toBe(false)
         expect(response.status).toBe(400)
     })
     test('should fail with no arrival, airport', async () => {
-        const response = await app.handle(createRequest({ to: 'LAX', date: '2022-01-01' }))
+        const response = await requestTest({ to: 'LAX', date: '2022-01-01' })
         expect(response.ok).toBe(false)
         expect(response.status).toBe(400)
     })
     test('should fail with no date', async () => {
-        const response = await app.handle(createRequest({ from: 'LAX', to: 'LAX' }))
+        const response = await requestTest({ from: 'LAX', to: 'LAX' })
         expect(response.ok).toBe(false)
         expect(response.status).toBe(400)
     })
 })
+
