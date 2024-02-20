a713f993ad0bfd6d84c6a5f74a152727a0bebc29
Jack Heuberger <jackheuberger5@gmail.com>
2024-02-19 23:21:48 -0600

    This should be multiple commits I'm sorry
    - Declared an interface for what functions all clients should have
      (probably a bad idea since we'll only have one client)
    
    - Wrote parameter/response types for the Seats.Aero API based on what
      they require
    
    - Wrote types to parse the return data from SeatsAero APIs

--- api/src/processors/requestProcessor.ts
+++ api/src/processors/requestProcessor.ts
@@ -1,3 +1,18 @@
-const handleRequest = () => { "Hello World!"}
+import { SeatsAero } from "../clients/Seats.Aero/client"
+import { SeatsCachedSearchParams, SEAT_CLASS } from "../clients/Seats.Aero/types";
+import { SearchParams } from "../clients/client_interface";
+
+
+const handleRequest = async (body: SearchParams) => {
+    const seatsAero = new SeatsAero(process.env.SEATS_API!)
+    const searchParams: SeatsCachedSearchParams = {
+        origin_airport: "JFK",
+        destination_airport: "LAX",
+        cabin: SEAT_CLASS.ECONOMY,
+        start_date: "2024-05-01",
+        end_date: "2024-05-01" // TODO: as of now, the end_date is not optional
+    }
+    await seatsAero.find_route(searchParams)
+}
 
 export { handleRequest };
